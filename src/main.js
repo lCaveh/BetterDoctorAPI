d5690d8e64bd77f5476b3199f911b031908f1a28
Brian Nelson <nelsonsbrian@gmail.com>
2018-10-19 08:24:30 -0700

    add initial files and folders

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+// const Promise = require('es6-promise').Promise;
+
+$(document).ready(function() {
+
+});
