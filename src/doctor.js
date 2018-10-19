0505fb7b87b4fa4f89063c9d27a6e3b21c6bf24a
Brian Nelson <nelsonsbrian@gmail.com>
2018-10-19 11:42:11 -0700

    add function to search for doctors by name. Refactor code so you can search by name, symptom, and all through 1 promse

--- src/doctor.js
+++ src/doctor.js
@@ -1,5 +1,20 @@
 export class Doctor {
 
+    getDoctorsByName(name) {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&user_key=${process.env.exports.apikey}`;
+            request.onload = function () {
+                if (this.status === 200) {
+                    resolve(request.response);
+                } else {
+                    reject(Error(request.statusText));
+                }
+            };
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
 
     getDoctorsBySymptom(symptom) {
         return new Promise(function (resolve, reject) {
--- src/main.js
+++ src/main.js
@@ -6,25 +6,35 @@ import { Doctor } from './doctor';
 
 $(document).ready(function () {
 
-
-    $('#symptomForm').submit(function (event) {
-        event.preventDefault();
-        let symptom = $('#symptom').val();
-        let allEvents = new Doctor();
-        let promise = allEvents.getDoctorsBySymptom(symptom);
+    $('#queryForm').submit(function (event) {
+        event.preventDefault();  
+        let allDoctors = new Doctor();
+        let inputQuery = $('#query').val();
+        let queryType = $('input[name=queryType]:checked').val();
+        console.log(`QueryType ${queryType}`);
+        let promise;
+        if (queryType === "Name") {            
+            promise = allDoctors.getDoctorsByName(inputQuery);          
+            $('#doctorTitle').text(`Doctors by ${queryType}:`);
+        } else if (queryType === "Symptom") {        
+            promise = allDoctors.getDoctorsBySymptom(inputQuery);
+            $('#doctorTitle').text(`Doctors by ${queryType}:`);            
+        } else {
+            promise = allDoctors.getDoctorsInSeattle();
+            $('#doctorTitle').text(`Doctors in Seattle`);            
+        }
 
         promise.then(function (response) {
             let body = JSON.parse(response);
             let data = body.data;
             console.log(data);
 
-            $('.doctorTitle').text(`Doctors in ${symptom}:`);
             if (data.length === 0) {
-                $('.doctorList').text(`No doctors matching the symptom ${symptom} were found.`);
+                $('#doctorList').text(`No doctors matching the symptom ${queryType} were found.`);
             } else {
-                $('.doctorList').empty();
+                $('#doctorList').empty();
                 for (let i = 0; i < data.length; i++) {
-                    $('.doctorList').append(`<br>${data[i].profile.first_name} ${data[i].profile.last_name}  ${data[i].practices[0].phones[0].number}  ${data[i].practices[0].visit_address.street}  ${data[i].practices[0].website}  ${data[i].practices[0].accepts_new_patients} `);
+                    $('#doctorList').append(`<br>${data[i].profile.first_name} ${data[i].profile.last_name}  ${data[i].practices[0].phones[0].number}  ${data[i].practices[0].visit_address.street}  ${data[i].practices[0].website}  ${data[i].practices[0].accepts_new_patients} `);
                 }
             }
         }, function (error) {
@@ -32,30 +42,4 @@ $(document).ready(function () {
 
         });
     });
-
-    $('#getAll').click(function () {
-        let allEvents = new Doctor();
-        let promise = allEvents.getDoctorsInSeattle();
-
-        promise.then(function (response) {
-            let body = JSON.parse(response);
-            let data = body.data;
-            console.log(data);
-
-            $('.doctorTitle').text(`Doctors in Seattle`);
-            if (data.length === 0) {
-                $('.doctorList').text(`No doctors were found.`);
-            } else {
-                $('.doctorList').empty();
-                for (let i = 0; i < data.length; i++) {
-                    $('.doctorList').append(`<br>${data[i].profile.first_name} ${data[i].profile.last_name}  ${data[i].practices[0].phones[0].number}  ${data[i].practices[0].visit_address.street}  ${data[i].practices[0].website}  ${data[i].practices[0].accepts_new_patients} `);
-                }
-            }
-        }, function (error) {
-            $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-
-        });
-    });
-
-
 });
